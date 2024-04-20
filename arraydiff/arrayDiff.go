03d1e17a62ec8cbb5c867f43afdbb49a8bb2147e
egathang <elyg3672@gmail.com>
2024-04-21 02:24:35 +0300

    New function

---
+++ arraydiff/arrayDiff.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// In this program, we are working with 2 arrays a and b both have integers.
+// We are subtracting a list from the other and returning the results.
+// It also removes all elements in a which are also present in b
+// If a value in b, all of its occurences are removed from a
+func ArrayDiff(a, b []int) []int {
+
+}
+
+func main() {
+	arr1 := 
+	fmt.Println(ArrayDiff())
+}
