fc0e1099c4c8a1215f5c098be22c1a86b264a206
egathang <elyg3672@gmail.com>
2024-04-29 09:39:07 +0300

    Update on the function

--- reversewords/reversewords.go
+++ reversewords/reversewords.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // This function takes an input of type string and reverses it.
 // The method showed here
 func ReverseWords(str string) string {
 	var newstring string
-	isSpace := false
-	for _, char := range str {
-		if char == ' ' && isSpace {
-			isSpace = true
-			newstring = ""
-		} else if !isSpace {
-			newstring = string(char) + newstring
+	//var revstring []string
+	substrings := strings.Fields(str)
+	for _, word := range substrings {
+		for i := len(word) - 1; i >= 0; i-- {
+			newstring = newstring + string(word[i]) 
 		}
 	}
-	isSpace = false
-
-	return string(newstring)
+	//newstring = strings.Join(revstring, " ")
+	return newstring
 }
 
 func main() {
