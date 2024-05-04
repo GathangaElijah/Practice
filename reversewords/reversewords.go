5b03140e387d75fb77868162655c813f962cb891
egathang <elyg3672@gmail.com>
2024-05-05 01:09:59 +0300

    Final reverse function

--- reversewords/reversewords.go
+++ reversewords/reversewords.go
@@ -6,18 +6,21 @@ import (
 )
 
 // This function takes an input of type string and reverses it.
-// The method showed here
+// The method showed here swaps the indexex of the substrings.
 func ReverseWords(str string) string {
-	var newstring string
-	//var revstring []string
-	substrings := strings.Fields(str)
-	for _, word := range substrings {
-		for i := len(word) - 1; i >= 0; i-- {
-			newstring = newstring + string(word[i]) 
+	var reversedStr []string
+	var finalStr string
+	subStr := strings.Fields(str)
+
+	for _, str := range subStr {
+		runes := []rune(str)
+		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+			runes[i], runes[j] = runes[j], runes[i]
 		}
+		reversedStr = append(reversedStr, string(runes))
+		finalStr = strings.Join(reversedStr, " ")
 	}
-	//newstring = strings.Join(revstring, " ")
-	return newstring
+	return finalStr
 }
 
 func main() {
